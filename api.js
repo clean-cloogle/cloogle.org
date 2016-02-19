4cdf4e7c4d861a0709f5e99b6e21b442388b418f
Camil Staps <info@camilstaps.nl>
2016-02-20 00:21:15 +0100

    Improved highlighting for class contexts

--- api.js
+++ api.js
@@ -7,22 +7,51 @@ var sresults = document.getElementById('search_results');
 function highlight(type) {
 	str = '';
 
-	var lex = [
-			[/^(.+)(::.*)/, 'name'],
-			[/^([a-z]+)(.*)/, 'typevar'],
-			[/^([A-Z]\w*)(.*)/, 'type'],
-			[/^(\/\/.*)(.*?)/, 'comment'],
-			[/^(\W)(.*)/, 'punctuation']
-		];
+	var lex = {
+		start: [
+			[/^(\s+)(.*)/, ['whitespace']],
+			[/^(.+)(::.*)/, ['funcname'], 'type']
+		],
+		type: [
+			[/^(\s+)(.*)/, ['whitespace']],
+			[/^([a-z]+)(.*)/, ['typevar']],
+			[/^([A-Z]\w*)(.*)/, ['type']],
+			[/^(\/\/.*)(.*?)/, ['comment']],
+			[/^(\|)(.*)/, ['punctuation'], 'context'],
+			[/^(\W)(.*)/, ['punctuation']]
+		],
+		context: [
+			[/^(\s+)(.*)/, ['whitespace']],
+			[/^(\/\/.*)(.*?)/, ['comment']],
+			[/^(\S+)(.*)/, ['classname'], 'contextType']
+		],
+		contextType: [
+			[/^(\s+)(.*)/, ['whitespace']],
+			[/^(\/\/.*)(.*?)/, ['comment']],
+			[/^(,)(.*)/, ['punctuation'], 'context'],
+			[/^([^\s,]+)(.*)/, ['typevar']]
+		]
+	};
 
+	state = 'start';
 	while (true) {
 		var found = false;
-		for (i in lex) {
-			if (type.match(lex[i][0])) {
-				parts = lex[i][0].exec(type);
-				str += '<span class="' + lex[i][1] + '">' + parts[1] + '</span>';
-				type = parts[2];
+		for (i in lex[state]) {
+			patt = lex[state][i][0];
+			clss = lex[state][i][1];
+			if (type.match(patt)) {
+				parts = patt.exec(type);
+				var j = 0;
+				for (k in clss) {
+					j = parseInt(k)+1;
+					str += '<span class="' + clss[k] + '">' + parts[j] + '</span>';
+				}
+				type = parts[j+1];
+
 				found = true;
+				if (lex[state][i].length > 2)
+					state = lex[state][i][2];
+
 				break;
 			}
 		}

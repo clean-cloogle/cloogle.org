f47fa56572bb68479aad2e151afeab837be85d43
Camil Staps <info@camilstaps.nl>
2016-02-20 00:24:18 +0100

    Allow partial function names; sort by levenshtein distance

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ define('STDENV_PATH', './stdenv/');
 define('PRE_MODULE', 
 	"/\s*(?:definition\s*|system\s*|implementation\s*)module\s+(\S+)\s*[\n;]/");
 define('PRE_FUNC', 
-	"/\s+%s\s+::\s+.*[\n;]/i");
+	'/^\s*(\S*%s\S*)\s*::.*$/mi');
 
 function search_doc(&$r, $name){
 	$files = glob(STDENV_PATH . "*.dcl", GLOB_NOSORT | GLOB_MARK);
@@ -17,17 +17,25 @@ function search_doc(&$r, $name){
 			$contents = file_get_contents($filepath);
 			$module = preg_match(PRE_MODULE, $contents, $modules) == 1 ?
 				$modules[1] : NULL;
-			if(preg_match(sprintf(PRE_FUNC, $name), $contents, $funcs) == 1){
-				foreach($funcs as $func){
+			$pattern = sprintf(PRE_FUNC, $name);
+			if(preg_match_all($pattern, $contents, $funcs) !== false){
+				$funcs = array_map(null, ...$funcs);
+				foreach(array_filter($funcs, 'count') as $func){
 					array_push($r, array(
 						"filename" => $filename,
-						"func" => trim($func),
-						"module" => $module));
+						"func" => trim($func[0]),
+						"module" => $module,
+						"distance" => levenshtein(
+							strtolower($name), strtolower($func[1]))));
 				}
 			}
 		}
 	}
 	return "Success";
+}
+
+function sort_results(&$r, $by='distance'){
+	usort($r, function($a, $b) use ($by) { return $a[$by] > $b[$by]; });
 }
 
 if($_SERVER['REQUEST_METHOD'] !== 'GET'){
@@ -43,6 +51,7 @@ if($_SERVER['REQUEST_METHOD'] !== 'GET'){
 } else {
 	$res = array();
 	$msg = search_doc($res, $_GET['str']);
+	sort_results($res);
 	if(!$res){
 		echo json_encode(array(
 			"return" => 127,

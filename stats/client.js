13b75ade36ad9a72d662bdb07af92676b8960ba5
Camil Staps <info@camilstaps.nl>
2016-05-21 11:17:52 +0200

    Automatically reload websocket connection on closing

--- stats/client.js
+++ stats/client.js
@@ -38,30 +38,62 @@ function updateChart() {
 	Chart.render('activity', data);
 }
 
-connection = new WebSocket('ws://' + window.location.hostname + ':31216/', 'cloogle-stats');
-connection.onopen = function() { console.log('Connection open'); };
-connection.onclose = function() { console.log('Connection closed'); };
-connection.onmessage = function(msg) {
-	console.log(msg.data);
-	var req = JSON.parse(msg.data);
-	var query = req.name + (req.unify ? ' :: ' + req.unify : '');
-	console.log(query);
+var open_timer;
+var is_open_message = false;
+function addConnectionCallbacks(connection) {
+	connection.onopen = function() {
+		is_open_message = true;
+		console.log('Connection open');
+		window.clearInterval(open_timer);
+	};
 
-	searches.splice(0, 0, query);
-	if (searches.length > 10)
-		searches.splice(searches.length - 1, searches.length);
-
-	var time = new Date().timeNow(false);
-	if (activity.length == 0 || activity[activity.length - 1].x != time) {
-		activity.push({ x: time, y: 0 });
+	function tryConnect() {
+		if (typeof connection.close != 'undefined') {
+			connection.close();
+		}
+		console.log('Attempting connection...');
+		connection = new WebSocket(
+				'ws://' + window.location.hostname + ':31216/',
+				'cloogle-stats');
+		addConnectionCallbacks(connection);
 	}
-	activity[activity.length - 1].y++;
 
-	updateSearches();
-	updateChart();
-};
+	connection.onclose = function() {
+		console.log('Connection closed');
+		window.clearTimeout(open_timer);
+		open_timer = window.setInterval(tryConnect, 10000);
+		tryConnect();
+	};
 
-activity[0] = { x: new Date().timeNow(false), y: -1 };
+	connection.onmessage = function(msg) {
+		console.log(msg.data);
+		var req = JSON.parse(msg.data);
+		var query = req.name + (req.unify ? ' :: ' + req.unify : '');
+		console.log(query);
+
+		searches.splice(0, 0, query);
+		if (searches.length > 10)
+			searches.splice(searches.length - 1, searches.length);
+
+		if (!is_open_message) {
+			var time = new Date().timeNow(false);
+			if (activity.length == 0 || activity[activity.length - 1].x != time) {
+				activity.push({ x: time, y: 0 });
+			}
+			activity[activity.length - 1].y++;
+		} else {
+			is_open_message = false;
+		}
+
+		updateSearches();
+		updateChart();
+	};
+}
+var connection = {};
+addConnectionCallbacks(connection);
+connection.onclose();
+
+activity[0] = { x: new Date().timeNow(false), y: 0 };
 
 window.setInterval(function(){
 	var date = new Date();

63b36944a91791ebe1d11f822f1a69d61b6e37e6
Camil Staps <info@camilstaps.nl>
2016-10-19 23:07:09 +0200

    minor fix

--- gc/gc.sh
+++ gc/gc.sh
@@ -8,7 +8,7 @@ cd "$CACHE_DIR"
 while :; do
 	n="$(($(ls -l | wc -l)-1))"
 	if [ "$n" -gt "$CACHE_SIZE" ]; then
-		ls -1tu | tail -n "$((n-CACHE_SIZE))" | xargs -n1 rm -v
+		ls -1tu | tail -n "$((n-CACHE_SIZE))" | xargs -P$(nproc) -r rm -v
 	else
 		echo "$n / $CACHE_SIZE cache entries used"
 	fi

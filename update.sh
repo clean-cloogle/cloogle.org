3a52b0e97ea8298cb84a3fb46622967d172a842c
Camil Staps <info@camilstaps.nl>
2017-03-05 12:00:53 +0100

    Add convenience update script

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Pulling new commits..."
+
+git checkout frontend/index.html
+git pull origin master
+git submodule init
+git submodule update
+
+echo "Updating containers..."
+
+docker-compose build
+docker-compose up -d
+
+echo "All done."
+
+echo
+read -p "Do you want to clear the caches? (y/[n]) " confirm
+case "$confirm" in
+	y|Y ) echo "Clearing the cache..."; sudo sh -c 'rm -f cache/lt/* cache/brief/*';;
+	* ) echo "Not clearing the cache.";;
+esac

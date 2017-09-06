474dfb27c950b62beaf39698ec730966b6a4fd7c
Camil Staps <info@camilstaps.nl>
2017-09-06 16:28:46 +0200

    Max/avg nr of unique visitors p/d added to longterm stats (#62)

--- frontend/stats/ajax/extremes.php
+++ frontend/stats/ajax/extremes.php
@@ -1,7 +1,7 @@
 <?php
 require_once('./conf.php');
 
-$sqlavg =
+$sqlavgreq =
 	"SELECT AVG(`number`) as avgn FROM
 	(SELECT COUNT(*) as number
 		FROM `log`
@@ -9,7 +9,7 @@ $sqlavg =
 			`responsecode` <> " . E_DOSPROTECT . " AND
 			`date` BETWEEN timestamp('$startTime') AND timestamp('$endTime')
 		GROUP BY DATE(`date`)) counts";
-$sqlmax =
+$sqlmaxreq =
 	"SELECT number as maxn, thedate as maxd FROM
 	(SELECT DATE(`date`) as thedate, COUNT(*) as number
 		FROM `log`
@@ -19,27 +19,60 @@ $sqlmax =
 		GROUP BY DATE(`date`)
 		ORDER BY number DESC, thedate DESC
 		LIMIT 1) counts";
+$sqlavgvis =
+	"SELECT AVG(number) as avgn FROM
+	(SELECT DATE(`date`) as thedate, COUNT(DISTINCT ip, useragent_id) as number
+		FROM `log`
+		WHERE
+			`responsecode` <> " . E_DOSPROTECT . " AND
+			`date` BETWEEN timestamp('$startTime') AND timestamp('$endTime')
+		GROUP BY DATE(`date`)) counts;";
+$sqlmaxvis =
+	"SELECT number as maxn, thedate as maxd FROM
+	(SELECT DATE(`date`) as thedate, COUNT(DISTINCT ip, useragent_id) as number
+		FROM `log`
+		WHERE
+			`responsecode` <> " . E_DOSPROTECT . " AND
+			`date` BETWEEN timestamp('$startTime') AND timestamp('$endTime')
+		GROUP BY DATE(`date`)
+		ORDER BY number DESC, thedate DESC
+		LIMIT 1) counts;";
 
 $stmt = $db->stmt_init();
-if (!$stmt->prepare($sqlavg))
+if (!$stmt->prepare($sqlavgreq))
 	var_dump($stmt->error);
 $stmt->execute();
-$stmt->bind_result($avgn);
+$stmt->bind_result($avgr);
 $stmt->fetch();
 $stmt->close();
 
 $stmt = $db->stmt_init();
-if (!$stmt->prepare($sqlmax))
+if (!$stmt->prepare($sqlmaxreq))
 	var_dump($stmt->error);
 $stmt->execute();
-$stmt->bind_result($maxn, $maxd);
+$stmt->bind_result($maxrn, $maxrd);
+$stmt->fetch();
+$stmt->close();
+
+$stmt = $db->stmt_init();
+if (!$stmt->prepare($sqlavgvis))
+	var_dump($stmt->error);
+$stmt->execute();
+$stmt->bind_result($avgv);
+$stmt->fetch();
+$stmt->close();
+
+$stmt = $db->stmt_init();
+if (!$stmt->prepare($sqlmaxvis))
+	var_dump($stmt->error);
+$stmt->execute();
+$stmt->bind_result($maxvn, $maxvd);
 $stmt->fetch();
 $stmt->close();
 
 $results =
-	[ 'avgn' => $avgn
-	, 'maxn' => $maxn
-	, 'maxd' => $maxd
+	[ 'requests' => ['avgn' => $avgr, 'maxn' => $maxrn, 'maxd' => $maxrd]
+	, 'visitors' => ['avgn' => $avgv, 'maxn' => $maxvn, 'maxd' => $maxvd]
 	];
 
 header('Content-Type: text/javascript');
